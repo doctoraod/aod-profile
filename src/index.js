816ad95b4d247695723b102fc4ef175e4358d314
Suvichan Prasongphonchai <acspdoctoraod@gmail.com>
2017-12-17 17:57:18 +0700

    Add console.log

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
+
+console.log("%cอ่าว!! สวัสดีคุณนี่มัน Developer ตัวจริง ขอให้สนุกกับ Profile ของผม", "color: red; font-size: x-large");
